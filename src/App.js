d1f7c283aadb4f8f63304d3febb6d1b64796eb3f
Nieves Montoya <cunanan.nm@gmail.com>
2022-08-18 20:41:16 +0800

    Create homepage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css"
+import HomePage from "./components/HomePage/HomePage";
 import Login from "./components/Login/Login";
 import UserReg from "./components/Login/UserReg";
 import { LoginProvider } from "./contexts/LoginContext";
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/userreg" element={<UserReg />}/>
+        <Route path="/homepage" element={<HomePage />}/>
       </Routes>
     </LoginProvider>
   );
---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,7 @@
+const HomePage = () => {
+    return(
+        <></>
+    )
+}
+
+export default HomePage;
