43eb72fe709aca2e601b5e60d28aa19d9e6f45e6
Nieves Montoya <cunanan.nm@gmail.com>
2022-08-18 18:10:17 +0800

    Create Login.css file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Login from "./components/Login";
+import Login from "./components/Login/Login";
 
 function App() {
   return (
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "./Login.css"
 
 const Login = () => {
     const [userInput, setUserInput] = useState({email: '', password: ''});
