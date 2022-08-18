ee5e911e8209edf8fec161323b18983989e80888
MisterScrooge <shawnhumbert13@gmail.com>
2022-08-18 19:06:17 +0800

    'tried-getting-login-headers'

--- src/components/CreateChannel.js
+++ src/components/CreateChannel.js
@@ -1,13 +1,9 @@
-import { useState, useEffect } from "react";
 
-const LoginAsync = () => {
+export default CreateChannel = () => {
     const url = "http://206.189.91.54/api/v1";
     const [userInfo, setUserInfo] = useState({
-        email: '',
-        password: '',
-    })
-    const [loginHeaders, setLoginHeaders] = useState({
-        expiry: '',
+        name: '',
+        user_ids: [],
     })
 
     const signUp = async () => {
@@ -25,27 +21,22 @@ const LoginAsync = () => {
         for (let [key, value] of response.headers) {
             console.log(`${key} = ${value}`);
         }
-        setLoginHeaders({...response.headers});
-
     }
 
     const submitHandler = (e) => {
         e.preventDefault();
         signUp(userInfo)
     }
-    useEffect(() => {console.log("loginHeaders,", loginHeaders)}, [loginHeaders])
 
     return (
         <div>
             <form>
-                <label>Email: </label>
-                <input type="text" value={userInfo.email} onInput={e => setUserInfo({...userInfo, email: e.target.value})}></input>
-                <label>Pass: </label>
-                <input type="text" value={userInfo.password} onInput={e => setUserInfo({...userInfo, password: e.target.value})}></input>
+                <label>Channel name: </label>
+                <input type="text" value={userInfo.email} onInput={e => setUserInfo({...userInfo, name: e.target.value})}></input>
+                <label>My email: </label>
+                <input type="text" value={userInfo.password} onInput={e => setUserInfo({...userInfo, user_ids: push(e.target.value)})}></input>
                 <button onClick={submitHandler}>Submit</button>
             </form>
         </div>
     )
 }
-
-export default LoginAsync;
