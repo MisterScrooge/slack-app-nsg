e4b6bf4fb2482d2a62698f6c5db594a946be361f
Nieves Montoya <cunanan.nm@gmail.com>
2022-08-18 18:47:39 +0800

    Add border radius and temp background color

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,37 +5,39 @@ const Login = () => {
     const [userInput, setUserInput] = useState({email: '', password: ''});
 
     return (
-        <div className="form-con">
-            <h3>Log in</h3>
-            <p className="sub-info">Welcome Back! Please enter your details</p>
+        <div className="login-reg-con">
+            <div className="form-con">
+                <h3>Log in</h3>
+                <p className="sub-info">Welcome Back! Please enter your details</p>
 
-            <form className="login-reg-form">
-                <div className="form-inner-con">
-                    <label>Email</label>
-                    <input
-                        type="text"
-                        placeholder="Enter your email"
-                        value={userInput.email}
-                        onInput={e => setUserInput({email: e.target.value, password: userInput.password})}
-                    />
-                </div>
+                <form className="login-reg-form">
+                    <div className="form-inner-con">
+                        <label>Email</label>
+                        <input
+                            type="text"
+                            placeholder="Enter your email"
+                            value={userInput.email}
+                            onInput={e => setUserInput({email: e.target.value, password: userInput.password})}
+                        />
+                    </div>
 
-                <div className="form-inner-con">
-                    <label>Password</label>
-                    <input
-                        type="password"
-                        placeholder="••••••"
-                        value={userInput.password}
-                        onInput={e => setUserInput({email: userInput.email, password: e.target.value})}
-                    />
-                </div>
+                    <div className="form-inner-con">
+                        <label>Password</label>
+                        <input
+                            type="password"
+                            placeholder="••••••"
+                            value={userInput.password}
+                            onInput={e => setUserInput({email: userInput.email, password: e.target.value})}
+                        />
+                    </div>
 
-                <div className="form-inner-con">
-                    <button type="Submit" className="form-btn">Sign in</button>
-                </div>
-            </form>
+                    <div className="form-inner-con">
+                        <button type="Submit" className="form-btn">Sign in</button>
+                    </div>
+                </form>
 
-            <p className="sub-info">Don't have an account? Sign up</p>
+                <p className="sub-info">Don't have an account? Sign up</p>
+            </div>
         </div>
     )
 }
