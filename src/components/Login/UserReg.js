a8519f45deeb28916f350427c6e42f39f2ec6181
Nieves Montoya <cunanan.nm@gmail.com>
2022-08-18 19:57:08 +0800

    Remove Link imports

--- src/components/Login/UserReg.js
+++ src/components/Login/UserReg.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "./Login-Reg.css";
 
 const UserReg = () => {
