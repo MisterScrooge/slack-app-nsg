b68b6bb165cf7733bb03105d1d0612724c29f86f
Nieves Montoya <cunanan.nm@gmail.com>
2022-08-19 20:23:00 +0800

    Add chat-header with selected channels name

--- src/components/MainChat/MainChat.js
+++ src/components/MainChat/MainChat.js
@@ -1,9 +1,13 @@
+import { useContext } from "react";
+import { SelectedContext } from "../../contexts/SelectedContext";
 import "./MainChat.css";
 
 const MainChat = () => {
+    const {selected} = useContext(SelectedContext);
+
     return(
         <div className="main">
-
+            <div className="header chat-header">{selected.name}</div>
         </div>
     )
 }
