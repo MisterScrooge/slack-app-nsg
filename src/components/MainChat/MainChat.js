a225c8027a6e8b07adf33893e117df4b6e817507
Nieves Montoya <cunanan.nm@gmail.com>
2022-08-19 21:26:32 +0800

    Add retrieve messages feature

--- src/components/MainChat/MainChat.js
+++ src/components/MainChat/MainChat.js
@@ -1,13 +1,37 @@
 import { useContext, useEffect, useState } from "react";
+import { ChannelsContext } from "../../contexts/ChannelsContext";
+import { LoginHeaders } from "../../contexts/LoginContext";
 import { SelectedContext } from "../../contexts/SelectedContext";
 import "./MainChat.css";
 
 const MainChat = () => {
+    const {channels} = useContext(ChannelsContext);
     const {selected} = useContext(SelectedContext);
-    const [message, setMessage] = useState("");
+    const {loginHeaders} = useContext(LoginHeaders);
+    const [send, setSend] = useState("");
+    const [Messages, setMessages] = useState([]);
+    const url = "http://206.189.91.54/api/v1/";
+
+    const retrieveMessages = async () => {
+        const recClass = channels.findIndex(obj => obj.id === selected.id) !== -1 ? "Channel" : "User";
+
+        const response = await fetch(`${url}messages?receiver_id=${selected.id}&receiver_class=${recClass}`,  {
+            method: 'GET',
+            headers: {
+                'expiry': loginHeaders['expiry'],
+                'uid': loginHeaders['uid'],
+                'access-token': loginHeaders['access-token'],
+                'client': loginHeaders['client']
+            }
+        });
+
+        const data = await response.json();
+        console.log(data['data']);
+    }
 
     useEffect(() => {
-        setMessage("");
+        setSend("");
+        retrieveMessages();
     }, [selected]);
 
     return(
@@ -21,9 +45,9 @@ const MainChat = () => {
             <div className="send-div">
                 <input
                     type="text"
-                    value={message}
+                    value={send}
                     placeholder={"Message " + selected.name}
-                    onInput={e => setMessage(e.target.value)}
+                    onInput={e => setSend(e.target.value)}
                 />
 
                 <div className="send-btn">
