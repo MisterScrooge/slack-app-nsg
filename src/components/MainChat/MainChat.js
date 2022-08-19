f46e622d6babe3c5a805fe9b7f90d1f3701ce6be
Nieves Montoya <cunanan.nm@gmail.com>
2022-08-19 20:57:14 +0800

    Create UI for sending a message

--- src/components/MainChat/MainChat.js
+++ src/components/MainChat/MainChat.js
@@ -8,6 +8,22 @@ const MainChat = () => {
     return(
         <div className="main">
             <div className="header chat-header">{selected.name}</div>
+
+            <div className="messages-div">
+
+            </div>
+
+            <div className="send-div">
+                <input
+                    type="text"
+                    placeholder={"Message " + selected.name}
+                />
+
+                <div className="send-btn">
+                    <i class="fa-solid fa-paper-plane"></i>
+                    Send
+                </div>
+            </div>
         </div>
     )
 }
