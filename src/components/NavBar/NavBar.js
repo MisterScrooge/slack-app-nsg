8e8b8129d4a188b76891d989783c0e43ca70aaf8
Nieves Montoya <cunanan.nm@gmail.com>
2022-08-19 18:19:24 +0800

    Create nav-list, nav-item, nav-header classes

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,7 +3,15 @@ import "./NavBar.css"
 const NavBar = () => {
     return(
         <div className="nav">
+            <h3 className="header">Chats</h3>
 
+            <div className="nav-list">
+                <h5 className="nav-header">Channels</h5>
+            </div>
+
+            <div className="nav-list">
+                <h5 className="nav-header">Direct Messages</h5>
+            </div>
         </div>
     )
 }
