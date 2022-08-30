cde4963d60e4336253f0516d4c788cd8c66e76e2
Nieves Montoya <cunanan.nm@gmail.com>
2022-08-30 16:57:50 +0800

    Add send icon in member list

--- src/components/Popup/ChannelDetailsPopup/ChannelMembers.js
+++ src/components/Popup/ChannelDetailsPopup/ChannelMembers.js
@@ -1,11 +1,13 @@
 import { useContext } from "react";
 import { ChannelDetails } from "../../../contexts/ChannelsContext";
 import { UsersContext } from "../../../contexts/UsersContext";
+import { LoginInfo } from "../../../contexts/LoginContext";
 import { useNavigate } from "react-router-dom";
 
 const ChannelMembers = () => {
     const {users} = useContext(UsersContext);
     const {channelDetails} = useContext(ChannelDetails);
+    const {loginInfo} = useContext(LoginInfo);
     const ownerIdx = users.findIndex(user => user.id === channelDetails['owner_id']);
     const navigate = useNavigate();
 
@@ -18,12 +20,15 @@ const ChannelMembers = () => {
                     const idx = users.findIndex(user => user.id === member['user_id']);
                     const user = users[idx];
                     const isOwner = idx === ownerIdx;
+                    const isLoggedInUser = loginInfo['data']['email'] === user.email;
 
                     return(
                         <div key={"member" + i} className="member">
                             <div className="initial">{user.email[0].toUpperCase()}</div>
                             {user.email}
                             {isOwner && <i className="fa-solid fa-crown"></i>}
+
+                            {!isLoggedInUser && <i className="fa-solid fa-paper-plane"></i>}
                         </div>
                     )
                 })}
